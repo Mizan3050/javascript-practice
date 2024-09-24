35af6f1fe4fb18d07b6ac83368487facd37fe7e2
Meezan Shaikh <meezan@tradexa.co.in>
2024-09-24 22:58:21 +0530

    3 sum

--- random.js
+++ random.js
@@ -1,2 +1,42 @@
-const randomNumber = Math.floor(2+Math.random()*10);
-console.log(randomNumber);
+var threeSum = function(nums) {
+    nums.sort((a,b)=> a-b);
+    const answerArray = [];
+    console.log(nums);
+    if(nums.length < 3){
+        return answerArray;
+    }
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]>0){
+            break;
+        }
+        if(i > 0 && nums[i]===nums[i-1]){
+            continue;
+        }
+        let low = i+1;
+        let high = nums.length-1;
+
+        while(low<high){
+            const sum = nums[i]+nums[low]+nums[high];
+            console.log(i,low,high);
+            if(sum > 0){
+                high--;
+            } else if (sum < 0){
+                low++;
+            } else if(sum === 0){
+                answerArray.push([nums[i],nums[low],nums[high]]);
+                let lowestOccurrence = nums[low];
+                let hightestOccurence = nums[high];
+
+                while(low<high && nums[low]===lowestOccurrence){
+                    low++;
+                }
+                while (low<high && nums[high] === hightestOccurence){
+                    high--;
+                }
+            }
+        }
+    }
+    return answerArray;
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]))
